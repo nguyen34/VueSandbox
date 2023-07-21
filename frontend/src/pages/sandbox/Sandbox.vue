d7ed6bbbde8a853d4a32b8f729d4e5c7852fc894
nguyen34 <nguyenjohnson65@gmail.com>
2023-07-20 22:20:46 -0700

    Initialized Vue Sandbox Repo

---
+++ frontend/src/pages/sandbox/Sandbox.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container fluid>
+      <!-- Add your work here -->
+      <div>
+        Sup
+      </div>
+    </v-container>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'Sandbox',
+    components: {
+      //
+    },
+
+    data() {
+      return {
+        //
+      }
+    },
+
+    computed: {
+      //
+    },
+
+    watch: {
+      //
+    },
+
+    methods: {
+      //
+    }
+
+  };
+  </script>
+  
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <hello-world />
+</template>
+
+<script lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+
+  export default {
+    name: "Home",
+
+    components: {
+      HelloWorld
+    },
+
+    data() {
+      return {
+        //
+      }
+    },
+
+    computed: {
+      //
+    },
+
+    watch: {
+      //
+    },
+
+    methods: {
+      //
+    }
+
+  }
+</script>
